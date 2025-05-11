d51a16147f234349b95aecfaaf5ba5d7c8ba8ba6
ericksaun <ericksaun@outlook.com>
2025-05-11 13:58:50 -0500

    Implementar API ASP.NET Core con controladores y Docker
    Se han añadido múltiples archivos y configuraciones para implementar una API en ASP.NET Core, incluyendo controladores (`ClientesController`, `CuentasController`, `MovimientosController`), un manejador de excepciones y configuraciones de Docker.
    
    Se ha actualizado el archivo de solución para incluir nuevos proyectos y se ha modificado el `Dockerfile` para definir las etapas de construcción y publicación.
    
    Además, se han añadido configuraciones en `appsettings.json` para la conexión a la base de datos y la limitación de tasa de IP, así como configuraciones de CORS.
    
    Se han creado clases de modelo como `WeatherForecast` y se han añadido configuraciones de lanzamiento en `launchSettings.json`.
    
    Por último, se han incluido dependencias necesarias en el archivo de proyecto y se han realizado cambios en el archivo de configuración de desarrollo para establecer niveles de registro.

---
+++ src/Presentation/Presentation.ApplicationProgrammingInterface/Controllers/CuentasController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.AppProgrammingInt.ClientePersona.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CuentasController : ControllerBase
+    {
+    }
+}
